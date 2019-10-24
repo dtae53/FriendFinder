ae7c12200eec902fbd7cee9062f37366c2277667
Davontae <davontaeglenn@gmail.com>
2019-10-23 20:29:21 -0700

    Added Files

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Drake',
+		photo: 'https://img.bleacherreport.net/img/slides/photos/003/568/755/fe8aa42a05f2361f0c99367abd005bc7_crop_exact.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Spike Lee',
+		photo: 'https://usatftw.files.wordpress.com/2017/02/usp_nba__san_antonio_spurs_at_new_york_knicks_88736134.jpg?w=1000&h=600&crop=1',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Snoop Dogg',
+		photo: 'https://cdn.ebaumsworld.com/mediaFiles/picture/290921/662250.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Jack Nicholson',
+		photo: 'http://sportsnaut.com/wp-content/uploads/2014/05/038.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Mark Wahlberg',
+		photo: 'http://www3.pictures.zimbio.com/gi/Mark+Wahlberg+Super+Bowl+XLIX+New+England+CGvaAizdbVnl.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Jessica Alba',
+		photo: 'https://cdn-s3.si.com/s3fs-public/images/Jessica_Alba.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'https://www.essence.com/wp-content/uploads/2019/06/GettyImages-1148281627-1920x1080.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Rihanna',
+		photo: 'https://media.wmagazine.com/photos/5931b26d3cc2b0622bc242b3/master/pass/GettyImages-461605854_master.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kate Upton',
+		photo: 'https://i.dailymail.co.uk/i/pix/2017/10/22/05/458F574500000578-5004997-image-m-17_1508646426349.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Carrie Underwood',
+		photo: 'http://www3.pictures.zimbio.com/fp/Carrie+Underwood+Carrie+Underwood+Nashville+JtBgI82ThAdl.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
